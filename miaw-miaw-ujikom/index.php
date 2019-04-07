7771a560d20c75ef70a8dbb0e37cd07d24318a3d
Dafa "Darksushi" Hibrizi <kometdafa@gmail.com>
2019-04-08 05:45:02 +0700

    Add files via upload
    hahaha that sick im still in that

---
+++ miaw-miaw-ujikom/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/master.css">
+    <title>Food Cult</title>
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            <div class="logo-box"><p class="text-bg"> Food </p><p class="text-bg"> Cult </p></div>
+        </div>
+    </header>
+    <section class="sec-login">
+
+    </section>
+</body>
+</html>
